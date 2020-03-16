50b02b107ad06141688cdf617cb82903bd770946
HZero <jasonactions@foxmail.com>
2020-03-16 15:13:52 +0800

    device: modify device_class using device_add instead of devcie_create

--- device-demo/device_class/device_demo.c
+++ device-demo/device_class/device_demo.c
@@ -6,7 +6,7 @@
 
 struct class *class_demo;
 
-static struct device *dev_demo;
+static struct device dev_demo;
 static struct device dev_demo3_1;
 static struct device dev_demo3_2;
 static struct device dev_demo3_3;
@@ -39,25 +39,23 @@ static int __init device_demo_init(void)
 		printk(KERN_ERR "create class demo fail!");
 		return -1;
 	}
-	/*class not null and parent not null*/
-	dev_demo = device_create(class_demo, NULL, 0, NULL, "dev-demo");                                                                           
-        if (!dev_demo) {                                                                                                                                   
-                printk(KERN_ERR "device_create dev-demo fail!\n");                                                                                          
-		return -1;
-	}
-		
+	device_demo_init_and_add(&dev_demo, "dev-demo", NULL, NULL);
 	device_demo_init_and_add(&dev_demo3_1, "dev-demo_noClass-noParent", NULL, NULL);
-	device_demo_init_and_add(&dev_demo3_2, "dev-demo_parent", dev_demo, NULL);
+	device_demo_init_and_add(&dev_demo3_2, "dev-demo_parent", &dev_demo, NULL);
 	device_demo_init_and_add(&dev_demo3_3, "dev-demo_class", NULL, class_demo);
-	device_demo_init_and_add(&dev_demo3_4, "dev-demo_class-parent", dev_demo, class_demo);
+	device_demo_init_and_add(&dev_demo3_4, "dev-demo_class-parent", &dev_demo, class_demo);
 
 	return 0;
 }
 
 static void __exit device_demo_exit(void)
 {
+#ifdef USE_DEVICE_CREATE
 	if (dev_demo)
 		device_del(dev_demo);
+#else
+	device_del(&dev_demo);
+#endif
 	device_del(&dev_demo3_1);
 	device_del(&dev_demo3_2);
 	device_del(&dev_demo3_3);
